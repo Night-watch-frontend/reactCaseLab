cc44d22af956f8c047a5b55e992e7ede90efdf18
Sergey <murderer83@gmail.com>
2024-10-27 19:01:36 +0300

    Updates

---
+++ precache-manifest.892da4c5fcbbfdeabe43bbf789601c7e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e206ceb320c59f0642b74064bfd0e682",
+    "url": "/reactCaseLab/index.html"
+  },
+  {
+    "revision": "3f77be1b85292b468308",
+    "url": "/reactCaseLab/static/js/2.6b7333a4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactCaseLab/static/js/2.6b7333a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25593ba291fd30d97159",
+    "url": "/reactCaseLab/static/js/main.afae2dd7.chunk.js"
+  },
+  {
+    "revision": "e38d33c46c5713ad93b1",
+    "url": "/reactCaseLab/static/js/runtime-main.0aed07fa.js"
+  },
+  {
+    "revision": "98878a7fb6c4d11f006f5f0a48816bae",
+    "url": "/reactCaseLab/static/media/Kontur-Iconic-eco-20px.98878a7f.woff2"
+  },
+  {
+    "revision": "b811ce6cc66ff93ba6ba6dd225b475df",
+    "url": "/reactCaseLab/static/media/Kontur-Iconic-eco-20px.b811ce6c.eot"
+  },
+  {
+    "revision": "cc8e65cb2d3a893285466730d7b4f022",
+    "url": "/reactCaseLab/static/media/Kontur-Iconic-eco-20px.cc8e65cb.woff"
+  },
+  {
+    "revision": "7d850235c4b0a12aed1c0d83e489acd5",
+    "url": "/reactCaseLab/static/media/fallback_circle.7d850235.png"
+  },
+  {
+    "revision": "7890347bd63a549c5164f53487b8031f",
+    "url": "/reactCaseLab/static/media/fallback_circle_big.7890347b.png"
+  },
+  {
+    "revision": "dae3133efdcd89ac9e4d3335d2675b1d",
+    "url": "/reactCaseLab/static/media/fallback_circle_normal.dae3133e.png"
+  },
+  {
+    "revision": "7b359e7d9365a987d17524f81fc66fb5",
+    "url": "/reactCaseLab/static/media/font.7b359e7d.woff2"
+  },
+  {
+    "revision": "dc62e0fc93a9b5469860b6a22338562d",
+    "url": "/reactCaseLab/static/media/font.dc62e0fc.eot"
+  },
+  {
+    "revision": "f01e8d1506e8cb4cbb1f1c39dab9dab9",
+    "url": "/reactCaseLab/static/media/font.f01e8d15.woff"
+  }
+]);
